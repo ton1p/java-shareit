46a95ce24c500e83411a44f3e17a8953a160a7d8
ton1p <posohin.tosha@yandex.ru>
2024-08-10 14:19:48 +0300

    fix: remove jakarta annotations from entity

--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -1,8 +1,5 @@
 package ru.practicum.shareit.item;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,19 +10,11 @@ import lombok.NoArgsConstructor;
 public class Item {
     private Long id;
 
-    @NotNull
     private Long ownerId;
 
-    @NotNull
-    @NotEmpty
-    @NotBlank
     private String name;
 
-    @NotNull
-    @NotEmpty
-    @NotBlank
     private String description;
 
-    @NotNull
     private Boolean available;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,10 +1,6 @@
 package ru.practicum.shareit.user;
 
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,12 +11,7 @@ import lombok.NoArgsConstructor;
 public class User {
     private Long id;
 
-    @NotNull
-    @NotBlank
-    @NotEmpty
     private String name;
 
-    @NotNull
-    @Email
     private String email;
 }
